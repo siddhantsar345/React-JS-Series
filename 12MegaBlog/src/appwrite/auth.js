ff8d847589a7f88eed44f930169c55914c85406b
siddhantsar345 <siddhantsar345@gmail.com>
2024-08-26 10:50:10 +0530

    minor changes have been made.

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -47,7 +47,7 @@ export class AuthService{
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite session :: logout :: error",error);  
+            console.log("Appwrite service :: logout :: error",error);  
         }
      }
 }
