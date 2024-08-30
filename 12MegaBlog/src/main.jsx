40640ffee876d02017e54f04b0c02a27f2084fea
siddhantsar345 <siddhantsar345@gmail.com>
2024-08-30 10:36:40 +0530

    Bug fixes

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -5,6 +5,13 @@ import './index.css'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
 import { createBrowserRouter } from 'react-router-dom'
+import Home from './Pages/Home.jsx'
+import { AuthLayout,Login } from './components/index.js'
+import AddPost from './Pages/AddPost.jsx'
+import Signup from './Pages/Signup.jsx'
+import EditPost from './Pages/EditPost.jsx'
+import Post from './Pages/Post.jsx'
+import AllPosts from './Pages/AllPosts.jsx'
 
 const router=createBrowserRouter([
   {
